c42e044aadc8ae74fd972d68615093b96f764bf4
Renato Acosta <133240037+NieblaDev@users.noreply.github.com>
2024-12-11 19:42:44 -0300

    adaptaciones a mobiles

--- perfiles.js
+++ perfiles.js
@@ -1,23 +1,41 @@
-const logoutButton = document.querySelector('.logout-button');
+const loginButton = document.querySelector('.login-button');
+const mobileLoginButton = document.querySelector('.mobile-login-button');
 const userEmailElement = document.getElementById('user-email');
 const userEmail = sessionStorage.getItem('userEmail'); // Obtener el correo del almacenamiento
 
 if (userEmail) {
     userEmailElement.textContent = userEmail; // Mostrar el correo
-    logoutButton.textContent = 'Cerrar Sesión'; // Cambiar texto del botón
-    
-} else {
-    userEmailElement.textContent = '¿No tienes cuenta vinculada?';
-    logoutButton.textContent = 'Iniciar Sesión';
+    loginButton.textContent = 'Cerrar Sesión'; // Cambiar texto del botón
+    mobileLoginButton.textContent = 'Cerrar Sesión'; // Cambiar texto del botón
 }
 
-logoutButton.addEventListener('click', () => {
+
+loginButton.addEventListener('click', () => {
     sessionStorage.clear(); // Limpiar almacenamiento
     window.location.href = 'login.html'; // Redirigir al login
 });
-
 
 function selectProfile(profileName) {
     sessionStorage.setItem('selectedProfile', profileName);
     window.location.href = 'citas.html';
 }
+
+const menuButton = document.querySelector('.mobile-menu-button');
+const sidebar = document.querySelector('.sidebar');
+const overlay = document.querySelector('.overlay');
+
+mobileLoginButton.addEventListener('click', () => {
+    sessionStorage.clear(); // Limpiar almacenamiento
+    window.location.href = 'login.html'; // Redirigir al login
+});
+
+menuButton.addEventListener('click', () => {
+    sidebar.classList.toggle('active'); // Alterna la clase 'open'
+    overlay.classList.toggle('active'); // Muestra o esconde el overlay
+});
+
+overlay.addEventListener('click', () => {
+    sidebar.classList.remove('active'); // Cierra el menú
+    overlay.classList.remove('active'); // Esconde el overlay
+});
+
